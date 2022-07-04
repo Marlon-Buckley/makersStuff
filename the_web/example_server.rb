592a2279e854548b7520685203fe823cdbcbed53
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-04 13:06:20 +0100

    updating example server.rb

--- the_web/example_server.rb
+++ the_web/example_server.rb
@@ -4,19 +4,18 @@ server = TCPServer.new(2345)
 
 puts "The server is running. Open a new terminal to make requests."
 
-puts "Type 'quit' to close server"
-
-quit = gets.chomp.downcase
-
-if quit == "quit" 
-  exit
-end
 
 loop do
   Thread.start(server.accept) do |socket|
-    socket.puts "What do you say?"
-    they_said = socket.gets.chomp
-    socket.puts "You said: #{they_said}. Goodbye!"
+    loop do 
+      socket.puts "What do you say? (type 'quit' to exit)"
+      they_said = socket.gets.chomp
+      if they_said.downcase == "quit"
+        break
+      else
+        socket.puts "You said: #{they_said}. Goodbye!"
+      end
+    end
     socket.close
   end
 end
@@ -25,5 +24,3 @@ end
   #lsof -wni tcp:2345
 #kill pif for server
   #kill PID 
-
-#socket.close
