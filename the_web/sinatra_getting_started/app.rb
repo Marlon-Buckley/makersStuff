76d8d566912404cfa9454e49337ac621ffa6914b
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-06 10:42:06 +0100

    updating

--- the_web/sinatra_getting_started/app.rb
+++ the_web/sinatra_getting_started/app.rb
@@ -1,1 +1,18 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "This is a seceret shhhhh!!!"
+end
+
+get '/test' do
+  "This is a test!!!"
+end
+
+# get '/favicon.ico' do
+#   <link rel="icon" href="/favicon.ico" />
+# end
