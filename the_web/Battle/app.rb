ca33be254812de1c9d15e9597fe30d9b6ae83db4
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-09 19:26:08 +0100

    finishing up refactor

--- the_web/Battle/app.rb
+++ the_web/Battle/app.rb
@@ -1,26 +1,30 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
-  #enable :sessions
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $player1 = Player.new(params[:player_1_name])
-    $player2 = Player.new(params[:player_2_name])
-    redirect 'play'
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
+    redirect '/play'
   end
 
   get '/play' do
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    $player1.attack($player2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- the_web/Battle/spec/features/attack_spec.rb
+++ the_web/Battle/spec/features/attack_spec.rb
@@ -1,14 +1,13 @@
 feature 'Attacking' do
   scenario 'Attack player 2' do
     sign_in_and_play()
-    click_button 'Attack'
+    click_link 'Attack'
     expect(page).to have_content "Charlotte attacked Mittens"
   end
 
   scenario 'reduce player 2 HP by 10 HP' do
     sign_in_and_play()
-    click_button 'Attack'
-    #click_link 'OK'
+    click_link 'Attack'
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
     save_and_open_page
